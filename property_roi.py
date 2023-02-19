9a66c068941b6a4a2de0ef8335ea68a01fbafe2f
Cameron Lee <camdlee18@gmail.com>
2023-02-19 14:32:50 -0500

    first commit

---
+++ property_roi.py
@@ -0,0 +1,30 @@
+# Topics from this week:
+# RegEx - searching for patterns within texts, strings, files, etc - like filtering for email inputs
+# Lambda, Map, Filter, Recursion, Generators - 
+# In & Out of place algorithms, two pointers, linked lists, merge sort, 
+# Time & Space complexity, Data structures - Arrays, stacks, queues, linked lists
+
+
+# Assignment - Create a program that calculates Return on Investment for a property
+
+
+#----------INCOME---------
+# Rental Income = 2000
+# Total Monthly Income = 2000
+
+
+#----------EXPENSES---------
+
+# Total Monthly Expense = 1610
+
+
+#---------CASH FLOW---------
+# Dependent on Income and Expenses
+# Monthly Cash flow = Income - Expenses
+
+#---------CASH ON CASH ROI---------
+# Dependent on how much money we put into the deal
+    # Down payment, closing costs, renovation, mics.
+# Total investment at time
+# Annual Cash Flow = 12 * Monthly Cash Flow
+# Annual Cash Flow / Total Investment = Return on Investment 
